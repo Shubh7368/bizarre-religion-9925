1ce0216222f37d61e80b0bce834d5e96dbb384fb
Shubham Dattatray Adhane <adhaneshubham622@gmail.com>
2022-11-13 22:42:11 +0700

    add

--- shubham-traveling/src/App.js
+++ shubham-traveling/src/App.js
@@ -1,28 +1,35 @@
-//import logo from './logo.svg';
-import './App.css';
-import Navbar from "./components/Navbar/Navbar"
-import Footer from './components/Footer/Footer';
-import HomeMidSec from './components/HomeMidSec';
-import BookingCard from './components/HomePage/BookingCard/BookingCard';
-import BookingCardCar from './components/HomePage/BookingCard/BookingCardCar';
-import BookingCardFlight from './components/HomePage/BookingCard/BookingCardFlight';
-import Login from './components/forms/Login';
-//import MultipleInputs from "./components/forms/MultipleInputs"
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import HotelPreview from "./components/HotelPageIPreview/HotelPreview.jsx";
 
+import Multipleinputs from './components/forms/MultipleInputs.jsx';
+// import AllRoutes from "./Components/pages/AllRoutes"
+import {Routes,Route} from 'react-router-dom'
+import Login from './components/forms/Login';
+
+import WithSubnavigation from "./components/Navbar/Navbar";
+import HomeMidSec from "./components/HomeMidSec.jsx";
+import SearchPage from "./components/pageresult/SearchPage";
+import Payment from "./components/payment/Payment.jsx"
+import Dummy from "./components/Dummy";
 
 function App() {
   return (
     <div className="App">
-      < Navbar />
-      < HomeMidSec />
-      < Footer />
-      < BookingCard />
-      <BookingCardCar />
-      <BookingCardFlight />
-      <Login />
-      {/* //<MultipleInputs /> */}
+     <WithSubnavigation/>
+    <Routes>
+      <Route exact path="/" element={<HomeMidSec/>}></Route>
+      <Route exact path="/signin" element={<Multipleinputs/>}></Route>
+      <Route exact path="/login" element={<Login />}></Route>
+      <Route path="/result" element={<SearchPage/>}></Route>
+      <Route path="/hotelpreview" element={<HotelPreview/>} ></Route>
+      <Route path="/payment" element={<Payment/>}/>
+      <Route path="/ImagePage" element={<Dummy/>}/>
+    </Routes>
 
-      
+
+      <Footer/>
+
     </div>
   );
 }
--- shubham-traveling/src/index.js
+++ shubham-traveling/src/index.js
@@ -2,27 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-//import reportWebVitals from "./reportWebVitals";
-// import { Provider } from "react-redux";
-// import store from "./store/store";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import { ChakraProvider } from "@chakra-ui/react";
- import { BrowserRouter } from "react-router-dom";
-// import AppContextProvider from './components/Context/AppContext';
+import { BrowserRouter } from "react-router-dom";
+import AppContextProvider from './components/Context/AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ChakraProvider>
+  <React.StrictMode>
     <BrowserRouter>
-    
-    <App />
+      <ChakraProvider>
+        {/* <Provider store={store}> */}
+        <AppContextProvider>
+        <App />
+        </AppContextProvider>
+        {/* </Provider> */}
+      </ChakraProvider>
     </BrowserRouter>
-  </ChakraProvider>
-
-
-  
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
